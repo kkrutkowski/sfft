f4f20618d6e45bdc13add8b32f905cc5a954d37f
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2024-09-10 16:06:40 +0200

    Add files via upload

--- periodogram_b.hpp
+++ periodogram_b.hpp
@@ -10,12 +10,13 @@
 #include "utils/grid.hpp"
 #include "utils/readout.hpp"
 
-std::tuple<float, float, float> periodogram(FFTGrid &grid, std::filesystem::path in_file, int terms, FFT &fft, int &threadID){
+std::tuple<float, float, float> periodogram(FFTGrid &grid, std::filesystem::path in_file, int terms, FFT &fft, int &threadID, uint method){
 
 	star data; data.read(in_file);
 	output_data best_frequency;
 
-	best_frequency = rayleigh_fft_b(data, grid, terms, fft, threadID); //binned conditional nonuniformity, requires fixing weighting function
+	if (method == 4){best_frequency = rayleigh_fft_b(data, grid, terms, fft, threadID);} //binned conditional nonuniformity, requires fixing weighting function
+
 	float powers_average = best_frequency.sum_of_powers / double(grid.freq.size()); //calculates average power for the input data
 
 	std::tuple<double, float, float> output_tuple = std::make_tuple(best_frequency.frequency, best_frequency.amplitude, best_frequency.power);
