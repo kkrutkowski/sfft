16e6f151b980bf28c4511fa2d25557d5a18ade22
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2024-09-10 20:59:30 +0200

    Add files via upload

--- src/periodograms/rayleigh_rec_b.hpp
+++ src/periodograms/rayleigh_rec_b.hpp
@@ -8,7 +8,7 @@
 #include "../../include/mintrig.hpp"
 //#include "utils/convolution.hpp"
 
-output_data rayleigh_simd_b(const star &data, const FFTGrid &grid, FFT &fft, int &threadID) {
+output_data rayleigh_rec_b(const star &data, const FFTGrid &grid, FFT &fft, int &threadID) {
         output_data best_frequency;
 
    int n = data.x.size();
@@ -21,7 +21,11 @@ output_data rayleigh_simd_b(const star &data, const FFTGrid &grid, FFT &fft, int
    float wsum=0, wsum_inv=0, Y=0, norm = 0;
    float *t = (float *) aligned_alloc(32, n_simd * sizeof(float)), //single precision float representation of time
           *w = (float *) aligned_alloc(32, n_simd * sizeof(float)),
-          *wy = (float *) aligned_alloc(32, n_simd * sizeof(float));
+          *wy = (float *) aligned_alloc(32, n_simd * sizeof(float)),
+          *cosx = (float *) aligned_alloc(32, n_simd * sizeof(float)),
+          *cosdx = (float *) aligned_alloc(32, n_simd * sizeof(float)),
+          *sinx = (float *) aligned_alloc(32, n_simd * sizeof(float)),
+          *sindx = (float *) aligned_alloc(32, n_simd * sizeof(float));
 
    float max_power = 0;
    uint i = 0; uint k = 0;
@@ -49,18 +53,36 @@ output_data rayleigh_simd_b(const star &data, const FFTGrid &grid, FFT &fft, int
       norm += wy[i] * wy[i];
    }
 
+   for (i = 0; i < n; i++){
+      cosdx[i] = cos(2 * M_PI * grid.fstep * t[i]);
+      sindx[i] = sin(2 * M_PI * grid.fstep * t[i]);
+   }
+
+   for (i = n; i < n_simd; i++){
+      cosdx[i] = 0;
+      sindx[i] = 0;
+   }
+
    for (i = n; i < n_simd; ++i){w[i] = 0; t[i] = 0; wy[i] = 0;}
 
    __m256* wy_simd = reinterpret_cast<__m256*>(wy);
    __m256* t_simd = reinterpret_cast<__m256*>(t);
 
+   __m256* cosdx_simd = reinterpret_cast<__m256*>(cosdx);
+   __m256* sindx_simd = reinterpret_cast<__m256*>(sindx);
+
+   __m256* cosx_simd = reinterpret_cast<__m256*>(cosx);
+   __m256* sinx_simd = reinterpret_cast<__m256*>(sinx);
+
    for (k=1; k<grid.freq.size(); ++k){
       float C = 0;
       float S = 0;
+      __m256 tmp;
+      float tmp_float;
+
       __m256 C_simd = _mm256_set1_ps(0.0f);
       __m256 S_simd = _mm256_set1_ps(0.0f);
-      __m256 C_temp;
-      __m256 S_temp;
+
       float power;
 
       // Two loops below are FMA equivalents of the loop above
@@ -72,16 +94,34 @@ output_data rayleigh_simd_b(const star &data, const FFTGrid &grid, FFT &fft, int
       */
 
 
+      if (k % 64 == 1){
       for (i=0; i<n_iter; i++){
-         FTA::sincos_ps( _mm256_mul_ps(t_simd[i], _mm256_mul_ps(_mm256_set1_ps(grid.freq[k]), _mm256_set1_ps(twopi))), &S_temp, &C_temp);
-         C_simd = _mm256_fmadd_ps(wy_simd[i], C_temp, C_simd);
-         S_simd = _mm256_fmadd_ps(wy_simd[i], S_temp, S_simd);
+         FTA::sincos_ps( _mm256_mul_ps(t_simd[i], _mm256_mul_ps(_mm256_set1_ps(grid.freq[k]), _mm256_set1_ps(twopi))), &sinx_simd[i], &cosx_simd[i]);
+      }
+      }
+
+      for (i=0; i<n_iter; i++){
+         C_simd = _mm256_fmadd_ps(wy_simd[i], cosx_simd[i], C_simd);
+         S_simd = _mm256_fmadd_ps(wy_simd[i], sinx_simd[i], S_simd);
       }
 
       for (i=0; i<8; i++){
          C += reinterpret_cast<float*>(&C_simd)[i];
          S += reinterpret_cast<float*>(&S_simd)[i];
       }
+
+
+
+      if (k % 64 != 0){
+         for (i=0; i<n_iter; i++){
+            //tmp = cosx_simd[i] * cosdx_simd[i] - sinx_simd[i] * sindx_simd[i];
+            tmp = _mm256_fmsub_ps(cosx_simd[i], cosdx_simd[i], _mm256_mul_ps(sinx_simd[i], sindx_simd[i]));
+            //sinx_simd[i] = cosx_simd[i] * sindx_simd[i] + sinx_simd[i] * cosdx_simd[i];
+            sinx_simd[i] = _mm256_fmadd_ps(cosx_simd[i], sindx_simd[i], _mm256_mul_ps(sinx_simd[i], cosdx_simd[i]));
+            cosx_simd[i] = tmp;
+         }
+      }
+
 
          power = ((C*C) + (S*S)) / norm;
 
