323adfc64ad22083d8f925ba7737b9601ca63de3
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2024-09-10 16:24:27 +0200

    Add files via upload
    Optimize memory usage

--- periodograms/rayleigh_fft_b.hpp
+++ periodograms/rayleigh_fft_b.hpp
@@ -7,7 +7,7 @@
 #include "../utils/readout.hpp"
 //#include "utils/convolution.hpp"
 
-output_data rayleigh_fft_b(const star &data, const FFTGrid &grid, int terms, FFT &fft, int &threadID) {
+output_data rayleigh_fft_b(const star &data, const FFTGrid &grid, FFT &fft, int &threadID) {
         output_data best_frequency;
 
    int n = data.x.size();
@@ -16,8 +16,6 @@ output_data rayleigh_fft_b(const star &data, const FFTGrid &grid, int terms, FFT
    float *t = (float *) malloc(n * sizeof(float)), //single precision float representation of time
           *w = (float *) malloc(n * sizeof(float)),
           *wy = (float *) malloc(n * sizeof(float));
-
-   float *powers = (float *) calloc(grid.freq.size(), sizeof(float));
 
    float max_power = 0;
    int i = 0; int k = 0;
@@ -47,19 +45,18 @@ output_data rayleigh_fft_b(const star &data, const FFTGrid &grid, int terms, FFT
    //norm /= float(n);
 
 
-   for(i=1; i <= terms; i++) {
-
    //std::cout << n << std::endl;
-   float* CS= fft.NDFT_I(t, wy, n, grid.fstep, threadID, float(i));
-   for (k=0; k< int(grid.freq.size() - 1); ++k) {powers[k] += float((CS[2*k] * CS[2*k]) + (CS[(2*k) + 1] * CS[(2*k) + 1])) / norm;} // Calibrate 'Z' value in Rayleigh's Z-test'
-   free(CS);}
-
-   for (k=1; k<grid.freq.size(); ++k){if (powers[k] > best_frequency.power)
+   float* CS= fft.NDFT_I(t, wy, n, grid.fstep, threadID, float(1));
+   float power;
+   for (k=1; k< int(grid.freq.size() - 1); ++k) {power = float((CS[2*k] * CS[2*k]) + (CS[(2*k) + 1] * CS[(2*k) + 1])) / norm;
+   if (power > best_frequency.power)
          {
-                  best_frequency.power = powers[k];
+                  best_frequency.power = power;
                   best_frequency.frequency = grid.freq[k];
-                     //std::cout << max_power << " " << best_freq << std::endl;
-               }}
+         }
+   }
 
-free(t); free(w); free(wy); free(powers);
+   free(CS);
+
+free(t); free(w); free(wy);
 return best_frequency;}
