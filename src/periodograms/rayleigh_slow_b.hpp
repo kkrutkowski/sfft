45bcd77ea25227bf291668fdd1d12290721ba554
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2024-09-10 16:39:18 +0200

    Add files via upload

---
+++ src/periodograms/rayleigh_slow_b.hpp
@@ -0,0 +1,72 @@
+#include <cmath>
+#include <cstring>
+
+#include "../utils/vertex.hpp"
+#include "../utils/periodograms.hpp"
+#include "../utils/fftgrid.hpp"
+#include "../utils/readout.hpp"
+//#include "utils/convolution.hpp"
+
+output_data rayleigh_slow_b(const star &data, const FFTGrid &grid, FFT &fft, int &threadID) {
+        output_data best_frequency;
+
+   int n = data.x.size();
+
+   float wsum=0, wsum_inv=0, Y=0, norm = 0;
+   float *t = (float *) malloc(n * sizeof(float)), //single precision float representation of time
+          *w = (float *) malloc(n * sizeof(float)),
+          *wy = (float *) malloc(n * sizeof(float));
+
+   float max_power = 0;
+   int i = 0; int k = 0;
+
+   for (i=0; i<n; ++i) {
+      /* weights */
+      w[i] = 1 / (data.dy[i] * data.dy[i]);
+      wsum += w[i];
+      t[i] = float(data.x[i]);
+   }
+
+   wsum_inv = 1 / wsum;
+
+   for (i=0; i<n; ++i) {
+      /* mean */
+      w[i] *= wsum_inv;                 /* normalised weights */
+      Y += w[i] * data.y[i];             /* Eq. (7) */
+   }
+
+   for (i=0; i<n; ++i) {
+      /* variance */
+      wy[i] = data.y[i] - Y;             /* Subtract weighted mean */
+      wy[i] *= w[i];                /* attach weights */
+      norm += wy[i] * wy[i];
+   }
+
+   //norm /= float(n);
+
+   //std::cout << n << std::endl;
+   /*
+   float* CS= fft.NDFT_I(t, wy, n, grid.fstep, threadID, float(i));
+   for (k=0; k< int(grid.freq.size() - 1); ++k) {powers[k] += float((CS[2*k] * CS[2*k]) + (CS[(2*k) + 1] * CS[(2*k) + 1])) / norm;} // Calibrate 'Z' value in Rayleigh's Z-test'
+   free(CS);
+   */
+
+   for (k=1; k<grid.freq.size(); ++k){
+      float C = 0;
+      float S = 0;
+      float power;
+      for (i=0; i<n; ++i) {
+         C += wy[i] * cos(2 * M_PI * grid.freq[k] * t[i]);
+         S += wy[i] * sin(2 * M_PI * grid.freq[k] * t[i]);
+      }
+         power = ((C*C) + (S*S)) / norm;
+
+         if (power > best_frequency.power)
+               {
+                  best_frequency.power = power;
+                  best_frequency.frequency = grid.freq[k];
+                     //std::cout << max_power << " " << best_freq << std::endl;
+               }}
+
+free(t); free(w); free(wy);
+return best_frequency;}
---
+++ src/utils/readout.hpp
@@ -0,0 +1,189 @@
+#ifndef READOUT_HPP
+#define READOUT_HPP
+
+#include <ostream>
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <algorithm> //for std::max
+#include <unordered_map>
+#include <mutex>
+
+#include "../../include/fast_float.h"
+//#include "../include/pdqsort.h"
+
+//#include "../include/yas/mem_streams.hpp"
+//#include "../include/yas/binary_iarchive.hpp"
+//#include "../include/yas/binary_oarchive.hpp"
+//#include "../include/yas/std_types.hpp"
+//#include "../include/yas/file_streams.hpp"
+
+
+
+
+struct star {
+    std::string id;
+    std::vector<double> x;
+    std::vector<float> y;
+    std::vector<float> dy;
+    //float median;
+
+    /*
+    inline void estimate_median(){
+
+    double y_sort[x.size()];
+    for (int i=0; i<x.size(); ++i) {y_sort[i] = y[i];}
+    pdqsort_branchless(&y_sort[0], &y_sort[x.size() - 1]);
+
+    median = y_sort[int(x.size() * 0.5)];
+    // std::cout << id << ".dat " << median << std::endl; //print approximate median value for file
+
+    }
+    */
+
+    inline void read(const std::string& in_file) {
+        std::ifstream input_file(in_file);
+
+        if (input_file) {
+            static const auto BUFFER_SIZE = 16 * 1024;
+            char buf[BUFFER_SIZE + 1];
+            uintmax_t lines = 0;
+            std::vector<char> dataBuffer; // To store the loaded data
+
+            while (input_file) {
+                input_file.read(buf, BUFFER_SIZE);
+                size_t bytes_read = input_file.gcount();
+
+                if (bytes_read == 0) {
+                    break;
+                }
+
+                dataBuffer.insert(dataBuffer.end(), buf, buf + bytes_read); // Append data to buffer
+
+                for (size_t i = 0; i < bytes_read; ++i) {
+                    if (buf[i] == '\n') {
+                        ++lines;
+                    }
+                }
+            }
+
+            // Set up iterators for parsing from dataBuffer
+            char const* it = dataBuffer.data();
+            char const* end = it + dataBuffer.size();
+
+            double tempX;
+            float tempY;
+            float tempDY;
+
+            while (it < end) {
+                auto temp = fast_float::from_chars(it, end, tempX);
+
+                if (temp.ec != std::errc()) {
+                    std::cout << "Parsing failed" << std::endl;
+                    break;
+                }
+                if(temp.ptr[0] != ' ' && temp.ptr[0] != '\n' ) {
+                    std::cout << "Unexpected delimiter" << std::endl;
+                }
+
+
+                temp = fast_float::from_chars(temp.ptr + 1, end, tempY);
+
+                if (temp.ec != std::errc()) {
+                    std::cout << "Parsing failed" << std::endl;
+                    break;
+                }
+
+                if(temp.ptr[0] != ' ' && temp.ptr[0] != '\n' ) {
+                    std::cout << "Unexpected delimiter" << std::endl;
+                }
+
+
+                temp = fast_float::from_chars(temp.ptr + 1, end, tempDY);
+
+                if (temp.ec != std::errc()) {
+                    std::cout << "Parsing failed" << std::endl;
+                    break;
+                }
+
+                if(temp.ptr[0] != ' ' && temp.ptr[0] != '\n' ) {
+                    std::cout << "Unexpected delimiter" << std::endl;
+                }
+
+
+                it = temp.ptr + 1;
+
+                x.push_back(tempX);
+                y.push_back(tempY);
+                dy.push_back(tempDY);
+            }
+
+            id = in_file.substr(in_file.find_last_of('/') + 1, in_file.find_last_of('.') - in_file.find_last_of('/') - 1); // Add star id to the struct
+        } else {
+            std::cout << "File cannot be opened" << std::endl;
+        }
+
+        input_file.close();
+    }
+
+    template <typename Archive>
+    inline void serialize(Archive& ar) {
+    ar & id & x & y & dy;
+    }
+};
+
+
+
+
+/*
+struct photometry {
+    std::vector<star> stars;
+    std::unordered_map<std::string, int> id;
+    std::mutex mutex;
+
+    template <typename Archive>
+    void serialize(Archive& ar) {
+        ar & stars & id;
+    }
+
+
+    inline void add(const star &star) {
+    std::lock_guard<std::mutex> lock(mutex);
+
+    stars.push_back(star);
+    id[star.id] = stars.size() - 1;
+    }
+
+    // Deserialize and load the photometry struct from file
+    void load(const std::string& file) {
+        const char* file_cstr = file.c_str();
+
+        yas::file_istream fis(file_cstr);
+        yas::binary_iarchive<yas::file_istream> ia(fis);
+
+        ia & *this;
+    }
+
+    void load_dat(std::string in_dir){
+        std::vector<std::filesystem::path> files;
+        for (auto& entry : std::filesystem::directory_iterator(in_dir)) {
+            if (entry.is_regular_file() && entry.path().extension() == ".dat") {
+                files.push_back(entry.path());
+            }
+        }
+
+        #pragma omp parallel for
+        for (unsigned int i = 0; i < files.size(); i++) {
+            star data;
+            data.read(files[i]);
+            add(data);}
+    }
+};
+*/
+
+
+#endif
